888c20ab07037a9ac1e73a4f71641c85a13f87a1
Vincent Truchot <muskacirca@gmail.com>
2015-10-12 22:18:42 +0200

    Deploy

--- public/components/auth.js
+++ public/components/auth.js
@@ -1,11 +1,22 @@
 module.exports = {
   login(email, pass, cb) {
+    cb = arguments[arguments.length - 1]
+    if (localStorage.token) {
+      console.log(localStorage.token)
+      if (cb) {
+        cb(true)
+      }
+      this.onChange(true)
+      return
+    }
     pretendRequest(email, pass, (res) => {
       if (res.authenticated) {
         localStorage.token = res.token
-        console.log("authenticated")
+        if (cb) cb(true)
+        this.onChange(true)
       } else {
-
+        if (cb) cb(false)
+        this.onChange(false)
       }
     })
   },
@@ -21,7 +32,7 @@ module.exports = {
   },
 
   loggedIn() {
-    return !!localStorage.token
+    return localStorage.token != undefined
   },
 
   onChange() {}
--- public/components/login.js
+++ public/components/login.js
@@ -1,5 +1,6 @@
 import React from 'react'
-var Button  = ReactBootstrap.Button;
+import Router from 'react-router'
+var Button = ReactBootstrap.Button;
 var PageHeader = ReactBootstrap.PageHeader;
 import auth from './auth.js'
 
@@ -7,60 +8,85 @@ const wellStyles = {maxWidth: 400, margin: '0 auto 10px'};
 
 class LoginBox extends React.Component {
 
-  render(){
-    return(
-      <div style={wellStyles}>
-        <Header label="Please login"/>
-        <LoginForm />
-      </div>);
-  }
+    constructor(props) {
+        super(props)
+        console.log(props)
+        this.history = props.history
+    }
+
+    render() {
+        return (
+            <div style={wellStyles}>
+                <Header label="Please login"/>
+                <LoginForm history={this.history}/>
+            </div>);
+    }
 }
 
 class Header extends React.Component {
 
-  render() {
-    return <PageHeader>{this.props.label}</PageHeader>
-  }
+    render() {
+        return <PageHeader>{this.props.label}</PageHeader>
+    }
 }
 
 class LoginForm extends React.Component {
 
-  handleSubmit(e) {
-    e.preventDefault();
+    constructor(props) {
+        super(props);
+        this.history = props.history,
+            console.log("history in LoginForm: " + props.history);
+        this.state = {error: false}
+    }
 
-    var email = React.findDOMNode(this.refs.loginField).value
-    var pass = React.findDOMNode(this.refs.passwordField).value
+    handleSubmit(e) {
+        e.preventDefault();
 
-    console.log("email: " + email)
-    console.log("pass: " + pass)
+        var email = React.findDOMNode(this.refs.loginField).value
+        var pass = React.findDOMNode(this.refs.passwordField).value
 
-    auth.login(email, pass)
+        console.log("email: " + email)
+        console.log("pass: " + pass)
 
+        auth.login(email, pass, (loggedIn) => {
+            if (!loggedIn) {
+                console.log("not authenticated")
+                return this.setState({error: true})
+            }
 
-    return;
-  }
+            console.log("authenticated, redirecting to profile");
+            this.history.replaceState(null, '/profile')
+        })
 
-  render() {
-    return (
-      <form className="commentForm" onSubmit={this.handleSubmit.bind(this)}>
-        <div className="input-group">
-          <span className="input-group-addon" id="basic-addon1">@</span>
-          <input type="text" className="form-control" ref="loginField" placeholder="login" aria-describedby="basic-addon1" />
-        </div>
-        <div className="input-group">
-          <span className="input-group-addon" id="basic-addon1">?</span>
-          <input type="password" className="form-control" ref="passwordField" placeholder="password" aria-describedby="basic-addon1" />
-        </div>
-        <SubmitButton label="Login"/>
-      </form>
-    );
-  }
+        return;
+    }
+
+    render() {
+        return (
+            <form className="commentForm" onSubmit={this.handleSubmit.bind(this)}>
+                <div className="input-group">
+                    <span className="input-group-addon" id="basic-addon1">@</span>
+                    <input type="text" className="form-control" ref="loginField" placeholder="login"
+                           aria-describedby="basic-addon1"/>
+                </div>
+                <div className="input-group">
+                    <span className="input-group-addon" id="basic-addon1">?</span>
+                    <input type="password" className="form-control" ref="passwordField" placeholder="password"
+                           aria-describedby="basic-addon1"/>
+                </div>
+                <SubmitButton label="Login"/>
+                {this.state.error && (
+                    <p>Bad login information</p>
+                )}
+            </form>
+        );
+    }
 }
 
 class SubmitButton extends React.Component {
-  render() {
-    return <button type="submit" className="btn btn-default primary">{this.props.label}</button>
-  }
+    render() {
+        return <button type="submit" className="btn btn-default primary">{this.props.label}</button>
+    }
 }
 
 export default LoginBox
