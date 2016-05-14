5b4bdd4cf1f096fab0fc85ee9c2727d5c0807893
Vincent Truchot <muskacirca@gmail.com>
2016-05-14 23:08:25 +0200

    added db-migrate

---
+++ migrations/sqls/20160514205745-alter-coomments-tables-up.sql
@@ -0,0 +1,2 @@
+ALTER TABLE  `lrds`.`itemComments` ADD author VARCHAR(255);
+ALTER TABLE  `lrds`.`eventComments` ADD author VARCHAR(255);
---
+++ migrations/sqls/20160514210354-test-up.sql
@@ -0,0 +1,4 @@
+/* Replace with your SQL commands */
+
+UPDATE lrds.eventComments SET author = 'lrds';
+UPDATE lrds.itemComments SET author = 'lrds';
