5b43252e1b4119122ae70bd585ba3fd264f23101
Vincent Truchot <muskacirca@gmail.com>
2016-05-20 01:47:23 +0200

    add proc stock and data to db-migrate

---
+++ migrations/sqls/20160519234611-add-proc-stock-up.sql
@@ -0,0 +1,163 @@
+DELIMITER //
+
+CREATE PROCEDURE lrds.add_item(
+  IN reference VARCHAR(255),
+  IN isInStock BOOLEAN,
+  IN modelName VARCHAR(255),
+  IN stateName VARCHAR(255)
+)
+  BEGIN
+
+    SET @modelId := (SELECT id FROM models WHERE name = modelName);
+    SET @stateId := (SELECT id FROM states WHERE name = stateName);
+
+    INSERT INTO items (reference, isInStock, modelId, stateId, createdAt, updatedAt) VALUES(reference, isInStock, @modelId, @stateId, now(), now());
+
+  END //
+
+CREATE PROCEDURE lrds.add_event(
+  IN eventName VARCHAR(255),
+  IN description TEXT,
+  IN startDate DATETIME,
+  IN endDate DATETIME
+)
+  BEGIN
+
+    INSERT INTO events (name, description, startDate, endDate, createdAt, updatedAt) VALUES(eventName, description, startDate, endDate, now(), now());
+
+  END //
+
+CREATE PROCEDURE lrds.bind_item_to_event(
+  IN eventName VARCHAR(255),
+  IN itemName VARCHAR(255)
+)
+  BEGIN
+
+    SET @eventId := (SELECT id FROM events WHERE name = eventName);
+    SET @itemId := (SELECT Id FROM items WHERE reference = itemName);
+
+    INSERT INTO reservedItems (eventId, itemId) VALUES(@eventId, @itemId);
+
+  END //
+
+CREATE PROCEDURE lrds.add_event_comment(
+  IN eventName VARCHAR(255),
+  IN itemReference VARCHAR(255)
+)
+  BEGIN
+
+    SET @eventId := (SELECT id FROM events WHERE name = eventName);
+
+    INSERT INTO eventComments (text, eventId, author, createdAt, updatedAt) VALUES(text, @eventId, 'lrds', now(), now());
+
+  END //
+
+
+CREATE PROCEDURE lrds.add_brand(
+  IN name VARCHAR(255),
+  IN description VARCHAR(255)
+)
+  BEGIN
+
+    INSERT INTO brands (name, description) VALUES(name, description);
+
+  END //
+
+
+CREATE PROCEDURE lrds.add_model(
+  IN modelName VARCHAR(255),
+  IN description VARCHAR(255),
+  IN brandName VARCHAR(255)
+)
+  BEGIN
+
+    SET @brandId := (SELECT id FROM brands WHERE name = brandName);
+
+    INSERT INTO models (name, description, brandId) VALUES(modelName, description, @brandId);
+
+  END //
+
+
+
+CREATE PROCEDURE lrds.add_domain(
+  IN name VARCHAR(255),
+  IN description VARCHAR(255)
+)
+  BEGIN
+
+    INSERT INTO domains (name, description) VALUES(name, description);
+
+  END //
+
+CREATE PROCEDURE lrds.add_category(
+  IN name VARCHAR(255),
+  IN description VARCHAR(255)
+)
+  BEGIN
+
+    INSERT INTO categories (name, description) VALUES(name, description);
+
+  END //
+
+
+CREATE PROCEDURE lrds.add_subCategory(
+  IN subCategoryName VARCHAR(255),
+  IN description VARCHAR(255),
+  IN categoryName VARCHAR(255)
+)
+  BEGIN
+
+    SET @categoryId := (SELECT id FROM categories WHERE name = categoryName);
+    INSERT INTO subCategories (name, description, categoryId) VALUES(subCategoryName, description, @categoryId);
+
+  END //
+
+CREATE PROCEDURE lrds.bind_model_to_subCategory(
+  IN modelName VARCHAR(255),
+  IN subCategoryName VARCHAR(255)
+)
+  BEGIN
+
+    SET @modelId := (SELECT id FROM models WHERE name = modelName);
+    SET @subCategoryId := (SELECT Id FROM subCategories WHERE name = subCategoryName);
+    INSERT INTO modelCategory (modelId, subCategoryId) VALUES(@modelId, @subCategoryId);
+
+  END //
+
+CREATE PROCEDURE lrds.bind_model_to_domain(
+  IN modelName VARCHAR(255),
+  IN domainName VARCHAR(255)
+)
+  BEGIN
+
+    SET @modelId := (SELECT id FROM models WHERE name = modelName);
+    SET @domainId := (SELECT id FROM domains WHERE name = domainName);
+    INSERT INTO modelDomain (modelId, domainId) VALUES(@modelId, @domainId);
+
+  END //
+
+
+CREATE PROCEDURE lrds.add_item_comment(
+  IN text VARCHAR(255),
+  IN itemReference VARCHAR(255)
+)
+  BEGIN
+
+    SET @itemId := (SELECT id FROM items WHERE reference = itemReference);
+
+    INSERT INTO itemComments (text, itemId, author, createdAt, updatedAt) VALUES(text, @itemId, 'lrds', now(), now());
+
+  END //
+
+
+CREATE PROCEDURE lrds.add_state(
+  IN name VARCHAR(255),
+  IN severity INT
+)
+  BEGIN
+
+    INSERT INTO states (name, severity) VALUES(name, severity);
+
+  END //
+
+DELIMITER ;
---
+++ migrations/sqls/20160519234621-add-data-up.sql
@@ -0,0 +1,60 @@
+CALL lrds.add_domain('SCENE', 'tout ce qui attrait a la scene');
+CALL lrds.add_domain('STUDIO', 'tout ce qui attrait au studio');
+
+CALL lrds.add_category('MICRO', 'pour enregggistrer des trucs');
+CALL lrds.add_subCategory('DYNAMIQUE', 'pas besoin de 48V', 'MICRO');
+CALL lrds.add_subCategory('BASS', 'pour la grosse caisse ou la basse', 'MICRO');
+
+CALL lrds.add_brand('Shure', 'A microphone company');
+
+CALL lrds.add_model('SM 58', 'Un micro tout terrain', 'Shure');
+
+CALL lrds.bind_model_to_domain('SM 58', 'SCENE');
+CALL lrds.bind_model_to_domain('SM 58', 'STUDIO');
+CALL lrds.bind_model_to_subCategory('SM 58', 'DYNAMIQUE');
+
+CALL lrds.add_state('neuf', 1);
+CALL lrds.add_state('bon état', 2);
+CALL lrds.add_state('dernier soufle', 3);
+CALL lrds.add_state('a réparer', 4);
+
+CALL lrds.add_item('SHUSM58-1', true, 'SM 58', 'neuf');
+
+CALL lrds.add_item_comment('first comment', 'SHUSM58-1');
+CALL lrds.add_item_comment('second comment', 'SHUSM58-1');
+CALL lrds.add_item_comment('third comment', 'SHUSM58-1');
+
+CALL lrds.add_model('PG 52', 'Un micro pour les fréquences basse', 'Shure');
+CALL lrds.bind_model_to_subCategory('SM 58', 'BASS');
+CALL lrds.add_item('SHUPG52-1', true, 'PG 52', 'bon état');
+
+CALL lrds.add_item_comment('first comment', 'SHUPG52-1');
+CALL lrds.add_item_comment('second comment', 'SHUPG52-1');
+
+CALL lrds.add_category('CARTE SON', 'a utiliser avec des micros');
+CALL lrds.add_subCategory('ADAT', 'optical connection 8 IN/OUT', 'CARTE SON');
+CALL lrds.add_subCategory('USB', 'connexion en usb', 'CARTE SON');
+
+CALL lrds.add_brand('RME', 'A studio company');
+CALL lrds.add_model('FIREFACE UC', 'une carte son 8 entrée 8 sortie, ADAT, etc ...', 'RME');
+
+CALL lrds.bind_model_to_subCategory('FIREFACE UC', 'ADAT');
+CALL lrds.bind_model_to_subCategory('FIREFACE UC', 'USB');
+
+CALL lrds.add_item('RMEFIREUC-1', true, 'FIREFACE UC', 'a réparer');
+
+CALL lrds.add_event('Festival sur le pointes', 'gros festival à vitry sur seine', now(), DATE_ADD(NOW(), INTERVAL 2 HOUR));
+CALL lrds.bind_item_to_event('Festival sur le pointes', 'SHUSM58-1');
+CALL lrds.add_event_comment('Festival sur le pointes', 'event comment');
+
+INSERT INTO lrds.users (firstName, lastName, login, password, email, enabled)
+VALUES ('lrds', 'lrds', 'lrds', '4RCh2u5VgDjlglgZzKAZ5/hGdzcAuLz2EFIHqQcukgY=', 'lrds@lrds.com', 1)
+
+# 4RCh2u5VgDjlglgZzKAZ5/hGdzcAuLz2EFIHqQcukgY=   ==> lrds
+
+
+
+
+
+
+
