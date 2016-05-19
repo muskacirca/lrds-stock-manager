05a2f363f1386ac58e5dc1f98b0d25ef32d6b92e
Vincent Truchot <muskacirca@gmail.com>
2016-05-19 21:47:42 +0200

    fix calendar display on mobile and with a lot of events

--- src/frontend/components/calendar/CalendarColumn.js
+++ src/frontend/components/calendar/CalendarColumn.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import moment from 'moment'
+import _ from 'lodash'
 
 import {
     Link
@@ -13,32 +14,54 @@ class CalendarColumn extends React.Component {
     
     renderEventsList(events) {
         
-        return events.map((event, key) => {
+        let slicedArray = _.slice(events.map((event, key) => {
             return  <li key={"calendar-event-list-" + this.props.dayNumber + "-" + key}>
                         <Link to={"/event/" + event.node.id}>{event.node.name}</Link>
                     </li>
-        })
+        }), 0, 3);
+        
+        return events.length > 3 ? _.concat(slicedArray, "...") : slicedArray
+    }
+
+    handleEventBadgeClick() {
+        
+    }
+    
+    renderMoreButton() {
+        return  <div className="mobile-hide calendar-event-plus-wrapper">
+                    <div className="pointer calendar-event-plus"
+                         onClick={this.handleEventBadgeClick.bind(this)}>
+                        <i className="fa fa-plus-circle" aria-hidden="true"/>
+                    </div>
+                </div>
     }
     
     render() {
         
-        var dayNumber = this.props.dayNumber
-        var now = moment()
+        let dayNumber = this.props.dayNumber;
+        let now = moment();
         
-        var events = this.renderEventsList(this.props.events)
+        let events = this.renderEventsList(this.props.events);
         
-        var className = dayNumber == now.date() 
+        let className = dayNumber == now.date() 
                         && this.props.defaultDate.month() == now.month() 
-                        && this.props.defaultDate.year() == now.year() ? "red" :  ""
+                        && this.props.defaultDate.year() == now.year() ? "red" :  "";
+        
+        let moreButton = this.props.events.length > 3 ? this.renderMoreButton() : null;
         
         return  <div className="calendar-row-content calendar-days">
                     <div className={className + " calendar-days-tr-up"}>
                         {dayNumber}
                     </div>
                     <div className="calendar-days-content">
+
                         <ul className="mobile-hide">{events}</ul>
-                        <div className="hidden-md hidden-sm hidden-lg">
-                            <span className="badge">{events.length > 0 ? events.length : null}</span>
+                        {moreButton}
+                        <div className="calendar-event-count-wrapper hidden-md hidden-sm hidden-lg">
+                            <div className="pointer calendar-event-count"
+                                onClick={this.handleEventBadgeClick.bind(this)}>
+                                <span className="badge">{events.length > 0 ? events.length : null}</span>
+                            </div>
                         </div>
                     </div>
                 </div>
